5d7cdb50534ec2451fb42912c31626a34c901ff6
Tyler <48775431+Thymester@users.noreply.github.com>
2025-04-14 10:22:13 -0400

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from monitor.system_monitor import ResourceMonitor
-
-if __name__ == "__main__":
-    monitor = ResourceMonitor()
-    monitor.root.protocol("WM_DELETE_WINDOW", monitor.cleanup)
-    monitor.root.mainloop()
